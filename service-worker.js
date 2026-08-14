a658f7580fdcee15d41132cdf7a72571d61b4d0e
FKCTRADINGACADEMY <khan.fazul2@gmail.com>
2026-08-14 14:43:55 +0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,23 @@
-const CACHE_NAME = "kissan-fertilizer-v1";
+const CACHE_NAME = "kissan-fertilizer-v2";
 const ASSETS = [
   "./index.html",
   "./manifest.json",
   "./icon-192.png",
-  "./icon-512.png"
+  "./icon-512.png",
+  "https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js",
+  "https://www.gstatic.com/firebasejs/12.17.1/firebase-auth.js",
+  "https://www.gstatic.com/firebasejs/12.17.1/firebase-firestore.js"
 ];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) =>
+      Promise.all(
+        ASSETS.map((url) =>
+          cache.add(url).catch(() => {}) // don't fail install if one asset is unreachable
+        )
+      )
+    )
   );
   self.skipWaiting();
 });
