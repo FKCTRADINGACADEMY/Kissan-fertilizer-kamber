7867ed8c4b0674ac35b67f7aa609f16c2c7e3d8a
FKCTRADINGACADEMY <khan.fazul2@gmail.com>
2026-09-21 09:16:14 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Kissan Fertilizer SW — robust PWA update (old + new Chrome) */
-var SW_VERSION = '20260921n';
+var SW_VERSION = '20260921p';
 var CACHE_NAME = 'kissan-' + SW_VERSION;
 var PRECACHE = [
   './',
