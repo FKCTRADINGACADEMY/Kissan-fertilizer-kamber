d86698eef326919039aef79190b453619c7c5deb
FKCTRADINGACADEMY <khan.fazul2@gmail.com>
2026-09-23 17:27:22 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Kissan Fertilizer SW — auto cache version + network-first updates */
-var SW_VERSION = '20260923h';
+var SW_VERSION = '20260923i';
 var CACHE_NAME = 'kissan-' + SW_VERSION;
 var PRECACHE = [
   './',
